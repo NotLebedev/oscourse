032b92900070519a6e73f9c69342ffb01b9929ae
a.galustov <artem-itf@yandex.ru>
2021-11-23 16:07:50 +0300

    Lab 9 done.

--- llvm/asan/asan_platform.ujos.c
+++ llvm/asan/asan_platform.ujos.c
@@ -65,6 +65,11 @@ platform_abort() {
 static bool
 asan_shadow_allocator(struct UTrapframe *utf) {
     // LAB 9: Your code here
+    if (!(asan_internal_shadow_start <= utf->utf_fault_va && 
+        asan_internal_shadow_end >= utf->utf_fault_va))
+        return 0;
+
+    sys_alloc_region(curenv, utf->utf_fault_va, SHADOW_STEP, ALLOC_ONE);
     return 0;
 }
 #endif
